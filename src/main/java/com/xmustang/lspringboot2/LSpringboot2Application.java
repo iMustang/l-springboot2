581e3c2a85e175db5cd961eefa0f869dd58c7d97
xMustang <xMustang@foxmail.com>
2019-06-30 21:15:13 +0800

    添加注解的意义

--- src/main/java/com/xmustang/lspringboot2/LSpringboot2Application.java
+++ src/main/java/com/xmustang/lspringboot2/LSpringboot2Application.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.PropertySource;
 //@SpringBootApplication包含@EnableAutoConfiguration
 //@EnableAutoConfiguration告诉SpringBoot基于已经添加的jar依赖项，“猜”我们将如何想配置Spring。
 //自动配置被设计用来和“Starters”一起更好的工作,但这两个概念并不直接相关。可以自由挑选starter依赖项以外的jar包，springboot仍将尽力自动配置应用程序。
+//@SpringBootApplication包含@Configuration、@EnableAutoConfiguration、@ComponentScan，使用这3个注解也是可以启动项目的。
+//带有@Configuration的类是一个IOC容器的配置类
 @PropertySource("classpath:app.properties")
 //@ImportResource用于引入xml文件
 @EnableConfigurationProperties(PropertiesBean.class)
